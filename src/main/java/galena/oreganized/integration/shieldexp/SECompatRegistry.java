7542f92cb83778bab4a0d6fa6e47610e495a0eaf
Xaidee <xaideems@gmail.com>
2023-01-10 16:24:48 +1300

    Make compatible with Shield Expansion 1.1.0 and adjust Electrum Shield stats

--- src/main/java/galena/oreganized/integration/shieldexp/SECompatRegistry.java
+++ src/main/java/galena/oreganized/integration/shieldexp/SECompatRegistry.java
@@ -1,24 +1,23 @@
 package galena.oreganized.integration.shieldexp;
 
+import galena.oreganized.index.OItems;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ShieldItem;
 import net.minecraftforge.registries.RegistryObject;
-import org.infernalstudios.shieldexp.items.NewShieldItem;
-
-import static galena.oreganized.index.OItems.ITEMS;
-import static org.infernalstudios.shieldexp.init.ItemsInit.SHIELDS;
+import org.infernalstudios.shieldexp.init.ItemsInit;
 
 public class SECompatRegistry {
 
-    //movement speed factors are modified to align with the vanilla shield modifier, actual results are 75%, 90%, 60%, 40~%, and 20~% as intended
-    public static final RegistryObject<NewShieldItem> ELECTRUM_SHIELD = registerShield("electrum_shield", 640, 35, 0.60F, 0.70F, 4);
+    public static final RegistryObject<ShieldItem> ELECTRUM_SHIELD = registerShield("electrum_shield", 363);
 
-    public static RegistryObject<NewShieldItem> registerShield(String id, int durability, int blockTicks, float speedFactor, double damageBack, int parryTicks) {
-        RegistryObject shield = ITEMS.register(id, () -> new NewShieldItem((new Item.Properties()).durability(durability).tab(CreativeModeTab.TAB_COMBAT), blockTicks, speedFactor, damageBack, parryTicks));
-        SHIELDS.add(shield);
+    public static RegistryObject<ShieldItem> registerShield(String id, int durability) {
+        Item.Properties properties = (new Item.Properties()).durability(durability).tab(CreativeModeTab.TAB_COMBAT);
+
+        RegistryObject<ShieldItem> shield = OItems.ITEMS.register(id, () -> new ShieldItem(properties));
+        ItemsInit.SHIELDS.add(shield);
         return shield;
     }
-
     public static void register() {
 
     }
