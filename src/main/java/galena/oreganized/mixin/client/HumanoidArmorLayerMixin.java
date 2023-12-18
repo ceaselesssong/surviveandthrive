57529c3890cbf6f6ae7a9921dfb3ea03a33728a2
Niklas Widmann <nik@somethingcatchy.net>
2023-12-18 13:40:24 +0100

    use accessor instead of shadow since its unreliable

--- src/main/java/galena/oreganized/mixin/client/HumanoidArmorLayerMixin.java
+++ src/main/java/galena/oreganized/mixin/client/HumanoidArmorLayerMixin.java
@@ -6,75 +6,49 @@ import galena.oreganized.index.OItems;
 import net.minecraft.client.model.HumanoidModel;
 import net.minecraft.client.model.Model;
 import net.minecraft.client.renderer.MultiBufferSource;
-import net.minecraft.client.renderer.entity.RenderLayerParent;
 import net.minecraft.client.renderer.entity.layers.HumanoidArmorLayer;
-import net.minecraft.client.renderer.entity.layers.RenderLayer;
-import net.minecraft.client.renderer.texture.TextureAtlas;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.item.*;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.item.armortrim.ArmorTrim;
-import org.jetbrains.annotations.Nullable;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(HumanoidArmorLayer.class)
-public abstract class HumanoidArmorLayerMixin extends RenderLayer<LivingEntity, HumanoidModel<LivingEntity>> {
-
-
-    @Shadow @Final private TextureAtlas armorTrimAtlas;
-
-    public HumanoidArmorLayerMixin(RenderLayerParent p_117346_) {
-        super(p_117346_);
-    }
-
-    @Shadow protected abstract Model getArmorModelHook(LivingEntity entity, ItemStack itemStack, EquipmentSlot slot, HumanoidModel<? extends LivingEntity> model);
-
-    @Shadow protected abstract void renderTrim(ArmorMaterial p_289690_, PoseStack p_289687_, MultiBufferSource p_289643_, int p_289683_, ArmorTrim p_289692_, Model p_289663_, boolean p_289651_);
-
-
-    @Shadow protected abstract void setPartVisibility(HumanoidModel<? extends LivingEntity> p_117126_, EquipmentSlot p_117127_);
-
-    @Shadow protected abstract boolean usesInnerModel(EquipmentSlot p_117129_);
-
-
-
-    @Shadow public abstract ResourceLocation getArmorResource(Entity entity, ItemStack stack, EquipmentSlot slot, @Nullable String type);
-
-    @Shadow protected abstract void renderModel(PoseStack p_289664_, MultiBufferSource p_289689_, int p_289681_, ArmorItem p_289650_, Model p_289658_, boolean p_289668_, float p_289678_, float p_289674_, float p_289693_, ResourceLocation armorResource);
-
-    @Shadow protected abstract void renderGlint(PoseStack p_289673_, MultiBufferSource p_289654_, int p_289649_, Model p_289659_);
+public abstract class HumanoidArmorLayerMixin {
 
     @Inject(method = "renderArmorPiece", at = @At("HEAD"), cancellable = true)
     public void renderElectrumArmorPiece(PoseStack poseStack, MultiBufferSource bufferSource, LivingEntity entity, EquipmentSlot slot, int packedLight, HumanoidModel<? extends LivingEntity> model, CallbackInfo ci) {
+        var accessor = (HumanoidArmorLayerAccessor) this;
+        var self = (HumanoidArmorLayer) (Object) this;
+
         ItemStack itemStack = entity.getItemBySlot(slot);
         Item item = itemStack.getItem();
         if (item instanceof ElectrumArmorItem armorItem) {
             if (armorItem.getEquipmentSlot() == slot) {
-                this.getParentModel().copyPropertiesTo((HumanoidModel<LivingEntity>) model);
-                setPartVisibility(model, slot);
-                Model electrumArmorModel = getArmorModelHook(entity, itemStack, slot, model);
-                boolean usesInnerModel = usesInnerModel(slot);
-                renderModel(poseStack, bufferSource, packedLight, armorItem, electrumArmorModel, usesInnerModel, 1.0F, 1.0F, 1.0F, getArmorResource(entity, itemStack, slot, null));
+                var parent = (HumanoidModel<?>) self.getParentModel();
+                parent.copyPropertiesTo((HumanoidModel) model);
+                accessor.invokeSetPartVisibility(model, slot);
+                Model electrumArmorModel = accessor.invokeGetArmorModelHook(entity, itemStack, slot, model);
+                boolean usesInnerModel = accessor.invokeUsesInnerModel(slot);
+                accessor.invokeRenderModel(poseStack, bufferSource, packedLight, armorItem, electrumArmorModel, usesInnerModel, 1.0F, 1.0F, 1.0F, accessor.invokeGetArmorResource(entity, itemStack, slot, null));
 
                 Item baseArmorItem = (
                         item == OItems.ELECTRUM_HELMET.get() ? Items.IRON_HELMET :
                                 item == OItems.ELECTRUM_CHESTPLATE.get() ? Items.IRON_CHESTPLATE :
                                         item == OItems.ELECTRUM_LEGGINGS.get() ? Items.IRON_LEGGINGS :
                                                 Items.IRON_BOOTS
-                        );
+                );
 
-                Model baseArmorModel = getArmorModelHook(entity, new ItemStack(baseArmorItem), slot, model);
+                Model baseArmorModel = accessor.invokeGetArmorModelHook(entity, new ItemStack(baseArmorItem), slot, model);
 
-                ArmorTrim.getTrim(entity.level().registryAccess(), itemStack).ifPresent((armorTrim) -> this.renderTrim(armorItem.getMaterial(), poseStack, bufferSource, packedLight, armorTrim, baseArmorModel, usesInnerModel));
+                ArmorTrim.getTrim(entity.level().registryAccess(), itemStack).ifPresent((armorTrim) -> accessor.invokeRenderTrim(armorItem.getMaterial(), poseStack, bufferSource, packedLight, armorTrim, baseArmorModel, usesInnerModel));
                 if (itemStack.hasFoil()) {
-                    this.renderGlint(poseStack, bufferSource, packedLight, electrumArmorModel);
+                    accessor.invokeRenderGlint(poseStack, bufferSource, packedLight, electrumArmorModel);
                 }
                 ci.cancel();
             }
