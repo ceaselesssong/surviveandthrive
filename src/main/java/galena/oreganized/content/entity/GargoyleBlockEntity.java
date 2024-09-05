353e1912837f4d2531180110697b9a8b89b2bb44
Niklas Widmann <nik@somethingcatchy.net>
2024-09-05 12:20:43 +0200

    fix compat items, rework garoyle particles and scare mechanic

--- src/main/java/galena/oreganized/content/entity/GargoyleBlockEntity.java
+++ src/main/java/galena/oreganized/content/entity/GargoyleBlockEntity.java
@@ -1,5 +1,6 @@
 package galena.oreganized.content.entity;
 
+import galena.oreganized.Oreganized;
 import galena.oreganized.content.block.GargoyleBlock;
 import galena.oreganized.index.OBlockEntities;
 import galena.oreganized.index.OParticleTypes;
@@ -9,22 +10,28 @@ import galena.oreganized.network.OreganizedNetwork;
 import galena.oreganized.network.packet.GargoyleParticlePacket;
 import galena.oreganized.world.ScaredOfGargoyleGoal;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.particles.ParticleOptions;
+import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.NbtUtils;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.util.ParticleUtils;
 import net.minecraft.util.valueproviders.UniformInt;
 import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.MobType;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.AABB;
 import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.network.PacketDistributor;
+import org.checkerframework.checker.units.qual.C;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.Collection;
@@ -32,10 +39,13 @@ import java.util.Collection;
 public class GargoyleBlockEntity extends BlockEntity {
 
     private static final int COOLDOWN = 20 * 30;
+    public static final String GROWL_COOLDOWN_TAG = Oreganized.MOD_ID + ":gargoyle_use_cooldown";
 
     private int outputSignal = 0;
     private int updateCooldown = 0;
     private int growlCooldown = 0;
+
+    private ParticleOptions drippingFluid;
 
     public GargoyleBlockEntity(BlockPos pos, BlockState state) {
         super(OBlockEntities.GARGOYLE.get(), pos, state);
@@ -48,7 +58,14 @@ public class GargoyleBlockEntity extends BlockEntity {
 
     public static void tick(Level level, BlockPos pos, BlockState state, GargoyleBlockEntity be) {
         be.growlCooldown--;
+
+        if (be.updateCooldown % 2 == 0 && be.drippingFluid != null) {
+            GargoyleBlock.dripParticles(state, level, pos, level.random, be.drippingFluid);
+        }
+
         if (--be.updateCooldown > 0) return;
+
+        be.updateDripParticles(level, pos, state);
 
         var targets = getTargets(level, pos);
         var vec = Vec3.atCenterOf(pos);
@@ -70,6 +87,24 @@ public class GargoyleBlockEntity extends BlockEntity {
         be.updateCooldown = 10;
     }
 
+    private void updateDripParticles(Level level, BlockPos pos, BlockState state) {
+        for (int i = 1; i <= 2; i++) {
+            var targetPos = pos.below(i);
+            var targetState = level.getBlockState(targetPos);
+            var fluid = targetState.getFluidState();
+            if (!fluid.isEmpty()) {
+                drippingFluid = fluid.getDripParticle();
+                return;
+            }
+        }
+
+        if (level.isRainingAt(pos.above())) {
+            drippingFluid = ParticleTypes.DRIPPING_DRIPSTONE_WATER;
+        } else {
+            drippingFluid = null;
+        }
+    }
+
     public int getAnalogOutputSignal() {
         return outputSignal;
     }
@@ -88,6 +123,7 @@ public class GargoyleBlockEntity extends BlockEntity {
 
     public InteractionResult interact(Level level, BlockPos pos, @Nullable Player player, ItemStack stack, boolean simulate) {
         if (growlCooldown > 0) return InteractionResult.PASS;
+        if (player != null && player.getPersistentData().getInt(GROWL_COOLDOWN_TAG) > 0) return InteractionResult.PASS;
         if (!stack.is(OTags.Items.GARGOYLE_SNACK)) return InteractionResult.PASS;
 
         if (player == null || !player.getAbilities().instabuild) {
@@ -96,19 +132,29 @@ public class GargoyleBlockEntity extends BlockEntity {
 
         if (simulate) return InteractionResult.SUCCESS;
 
-        getTargets(level, pos).forEach(mob -> {
-            mob.getPersistentData().put(ScaredOfGargoyleGoal.AVOID_TAG_KEY, NbtUtils.writeBlockPos(pos));
-        });
+        getTargets(level, pos).forEach(mob -> scare(mob, pos));
 
         level.playSound(null, pos.getX(), pos.getY(), pos.getZ(), OSoundEvents.GARGOYLE_GROWL.get(), SoundSource.BLOCKS, 1.0F, 1.0F);
 
         growlCooldown = COOLDOWN;
+        if (player != null && !player.getAbilities().instabuild) {
+            player.getPersistentData().putInt(GROWL_COOLDOWN_TAG, COOLDOWN);
+        }
 
         if (!level.isClientSide) {
             OreganizedNetwork.CHANNEL.send(PacketDistributor.DIMENSION.with(level::dimension), new GargoyleParticlePacket(pos));
         }
 
         return InteractionResult.SUCCESS;
+    }
+
+    private void scare(Entity mob, BlockPos pos) {
+        var offset = mob.position().subtract(pos.getX(), pos.getY(), pos.getZ());
+        if (offset.length() < 4) {
+            var motion = offset.multiply(0.4, 0.2, 0.4);
+            mob.push(motion.x, motion.y, motion.z);
+        }
+        mob.getPersistentData().put(ScaredOfGargoyleGoal.AVOID_TAG_KEY, NbtUtils.writeBlockPos(pos));
     }
 
     public void spawnParticles() {
