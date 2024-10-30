5310b77be467b7ef98449b81cb322a4f526a0104
Niklas Widmann <nik@somethingcatchy.net>
2024-10-30 11:07:34 +0100

    add fallback recipes for things made from silver & lead

--- src/main/java/galena/doom_and_gloom/data/ORecipes.java
+++ src/main/java/galena/doom_and_gloom/data/ORecipes.java
@@ -12,12 +12,19 @@ import net.minecraft.data.recipes.RecipeCategory;
 import net.minecraft.data.recipes.ShapedRecipeBuilder;
 import net.minecraft.data.recipes.ShapelessRecipeBuilder;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.tags.TagKey;
 import net.minecraft.world.item.DyeItem;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.common.Tags;
+import net.minecraftforge.common.crafting.ConditionalRecipe;
+import net.minecraftforge.common.crafting.conditions.NotCondition;
+import net.minecraftforge.common.crafting.conditions.TagEmptyCondition;
 
 import java.util.function.Consumer;
+import java.util.function.Function;
 
 public class ORecipes extends ORecipeProvider {
 
@@ -27,14 +34,14 @@ public class ORecipes extends ORecipeProvider {
 
     @Override
     protected void buildRecipes(Consumer<FinishedRecipe> consumer) {
-        ShapedRecipeBuilder.shaped(RecipeCategory.TOOLS, OItems.BUSH_HAMMER.get())
-                .pattern("AA")
-                .pattern("B ")
-                .define('A', OTags.Items.INGOTS_LEAD)
-                .define('B', Tags.Items.RODS_WOODEN)
-                .unlockedBy("has_lead_ingot", has(OTags.Items.INGOTS_LEAD))
-                .unlockedBy("has_stick", has(Tags.Items.RODS_WOODEN))
-                .save(consumer);
+        withFallback(OTags.Items.INGOTS_LEAD, Tags.Items.INGOTS_COPPER, ingot ->
+                ShapedRecipeBuilder.shaped(RecipeCategory.TOOLS, OItems.BUSH_HAMMER.get())
+                        .pattern("AA")
+                        .pattern("B ")
+                        .define('A', ingot)
+                        .define('B', Tags.Items.RODS_WOODEN)
+                        .unlockedBy("has_stick", has(Tags.Items.RODS_WOODEN))
+        ).accept(consumer);
 
         vigilCandle(OBlocks.VIGIL_CANDLE, Blocks.CANDLE).save(consumer);
 
@@ -50,15 +57,36 @@ public class ORecipes extends ORecipeProvider {
                     .save(consumer, RecipeBuilder.getDefaultRecipeId(block.get()).withSuffix("_dyeing"));
         });
 
-        ShapedRecipeBuilder.shaped(RecipeCategory.DECORATIONS, OBlocks.SEPULCHER.get())
-                .pattern("# #")
-                .pattern("# #")
-                .pattern("###")
-                .define('#', OTags.Items.INGOTS_SILVER)
-                .unlockedBy("has_silver", has(OTags.Items.INGOTS_SILVER))
-                .save(consumer);
+        withFallback(OTags.Items.INGOTS_SILVER, Tags.Items.INGOTS_IRON, ingot ->
+                ShapedRecipeBuilder.shaped(RecipeCategory.DECORATIONS, OBlocks.SEPULCHER.get())
+                        .pattern("# #")
+                        .pattern("# #")
+                        .pattern("###")
+                        .define('#', ingot)
+        ).accept(consumer);
 
         compact(OBlocks.BONE_PILE.get().asItem(), Items.BONE).save(consumer);
         unCompact(Items.BONE, OBlocks.BONE_PILE.get().asItem()).save(consumer);
     }
+
+    private Consumer<Consumer<FinishedRecipe>> withFallback(TagKey<Item> prefer, TagKey<Item> fallback, Function<TagKey<Item>, RecipeBuilder> builder) {
+        return consumer -> {
+            var preferredRecipe = builder.apply(prefer).unlockedBy("has_ingredient", has(prefer));
+            var id = RecipeBuilder.getDefaultRecipeId(preferredRecipe.getResult());
+
+            ConditionalRecipe.builder()
+                    .addCondition(new NotCondition(new TagEmptyCondition(prefer.location())))
+                    .addRecipe(preferredRecipe::save)
+                    .generateAdvancement()
+                    .build(consumer, id);
+
+            ConditionalRecipe.builder()
+                    .addCondition(new TagEmptyCondition(prefer.location()))
+                    .addRecipe(builder.apply(fallback).unlockedBy("has_ingredient", has(fallback))::save)
+                    .generateAdvancement()
+                    .build(consumer, id.withSuffix("_fallback"));
+
+        };
+    }
+
 }
