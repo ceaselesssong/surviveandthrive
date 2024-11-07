11a05bc33ac0c23a5be13a7dc6526ac2f5b05fb6
ceaselesssong <tabi@mechner.com>
2024-11-06 21:11:51 -0500

    God fucking damn it. please

--- src/main/java/com/ceaselesssong/surviveandthrive/SurviveAndThrive.java
+++ src/main/java/com/ceaselesssong/surviveandthrive/SurviveAndThrive.java
@@ -27,7 +27,7 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 import org.slf4j.Logger;
-// GOD FUCKING DAMNIT
+
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(SurviveAndThrive.MODID)
 public class SurviveAndThrive
